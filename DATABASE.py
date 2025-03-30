edb1b1fa7008d7809cce9852e87b52fe2cb117eb
Suyash Pathak <pathaksuyash04113@gmail.com>
2025-03-30 20:48:28 +0530

    DATABASE.py

--- DATABASE.py
+++ DATABASE.py
@@ -7,14 +7,10 @@ conn = mysql.connector.connect(
     database="SV"
 )
 c = conn.cursor()
-# s="suyash"
-#NotNull="ALTER TABLE trip Modify Amount int NOT NULL"
-#NotNull1="ALTER TABLE trip Modify Total int NOT NULL"
-#c.execute(NotNull1)
+
 Trip_Creation = "create table SV(TripName varchar(50), Name varchar(50), Amount int not null ,Total int not null ,remark varchar(50))"
 c.execute(Trip_Creation)
-# Amount_Table="create table Amount(TripName varchar(50),Amount int)"
-# c.execute(Amount_Table)
+
 conn.commit()
 c.close()
 conn.close()
